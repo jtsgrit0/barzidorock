2e0e82b6b1ead2eeadb7f5eedbe0e9cefc3bc494
Taewoo Kim <taewoo.kim@git-flow.com>
2026-06-15 01:55:00 +0900

    fix: Always show my location button

--- client/src/components/MapComponent.js
+++ client/src/components/MapComponent.js
@@ -159,13 +159,11 @@ function MapComponent({ venues, center, zoom, onFetchSchedule, scheduleContent, 
           </OverlayView>
         )}
 
-        {userLocation && (
           <div className="my-location-button-container">
             <button onClick={centerMapToUserLocation} className="my-location-button">
               <span></span>
             </button>
           </div>
-        )}
       </GoogleMap>
 
       {scheduleContent && ( // 스케줄 콘텐츠가 있는 경우에만 표시합니다.
