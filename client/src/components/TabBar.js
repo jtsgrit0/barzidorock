be39f628a606c1ee4bfcc39f1ee9ad4f34f804f4
Taewoo Kim <taewoo.kim@git-flow.com>
2026-06-15 01:21:08 +0900

    feat: Add icons to tab bar

--- client/src/components/TabBar.js
+++ client/src/components/TabBar.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faTicket, faCalendarAlt, faCog } from '@fortawesome/free-solid-svg-icons';
 import './TabBar.css';
 
 const TabBar = () => {
   return (
     <div className="tab-bar">
       <NavLink to="/" className="tab-item" activeClassName="active">
-        홈
+        <FontAwesomeIcon icon={faHome} />
+        <span>홈</span>
       </NavLink>
       <NavLink to="/tickets" className="tab-item" activeClassName="active">
-        티켓
+        <FontAwesomeIcon icon={faTicket} />
+        <span>티켓</span>
       </NavLink>
       <NavLink to="/schedule" className="tab-item" activeClassName="active">
-        스케줄
+        <FontAwesomeIcon icon={faCalendarAlt} />
+        <span>스케줄</span>
       </NavLink>
       <NavLink to="/options" className="tab-item" activeClassName="active">
-        옵션
+        <FontAwesomeIcon icon={faCog} />
+        <span>옵션</span>
       </NavLink>
     </div>
   );
