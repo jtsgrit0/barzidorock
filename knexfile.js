f2d58e230d3b8ed58bbcb8bf52d0f77640e0a368
Taewoo Kim <taewoo.kim@git-flow.com>
2026-06-17 10:00:28 +0900

    fix: correct production db path and add migration to start script

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     client: 'better-sqlite3',
     connection: {
-      filename: process.env.NODE_ENV === 'production' ? '/app/database.db' : './database.db'
+      filename: './database.db'
     },
     useNullAsDefault: true,
     migrations: {
@@ -12,8 +12,11 @@ module.exports = {
   production: {
     client: 'better-sqlite3',
     connection: {
-      filename: './database.db'
+      filename: '/app/database.db'
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
+    migrations: {
+      directory: './migrations'
+    }
   }
 };
