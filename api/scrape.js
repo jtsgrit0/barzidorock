2619fc5ced76187889f565ec547267eb2847c919
Taewoo Kim <taewoo.kim@git-flow.com>
2026-06-17 14:29:56 +0900

    Reconfigure vercel.json with correct paths

--- api/scrape.js
+++ api/scrape.js
@@ -2,9 +2,6 @@ const chrome = require('chrome-aws-lambda');
 const puppeteer = require('puppeteer-core');
 
 module.exports = async (req, res) => {
-  if (req.method === 'OPTIONS') {
-    return res.status(204).send('');
-  }
   if (req.method !== 'POST') {
     res.setHeader('Allow', ['POST']);
     return res.status(405).end(`Method ${req.method} Not Allowed`);
