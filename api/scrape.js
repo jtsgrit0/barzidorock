cc83f462ef9bc20edbb81e5901c1e7855d192cee
Taewoo Kim <taewoo.kim@git-flow.com>
2026-06-17 15:23:09 +0900

    Handle OPTIONS preflight request explicitly

--- api/scrape.js
+++ api/scrape.js
@@ -2,6 +2,9 @@ const chrome = require('chrome-aws-lambda');
 const puppeteer = require('puppeteer-core');
 
 module.exports = async (req, res) => {
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
   if (req.method !== 'POST') {
     res.setHeader('Allow', ['POST']);
     return res.status(405).end(`Method ${req.method} Not Allowed`);
