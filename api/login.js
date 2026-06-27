f40e4c01c366286b0debb437d0249fefb818e587
Taewoo Kim <taewoo.kim@git-flow.com>
2026-06-28 08:48:46 +0900

    fix: CORS 설정 오류 수정

--- api/login.js
+++ api/login.js
@@ -1,19 +1,16 @@
-module.exports = async (req, res) => {
-  // CORS 설정
-  const allowedOrigins = ['https://jtsgrit0.github.io', 'http://localhost:3000', 'https://barzidorock.vercel.app', 'https://barzidorock-4n8edt15l-jtsgrit0s-projects.vercel.app']; 
-  const origin = req.headers.origin; 
-  if (allowedOrigins.includes(origin)) { 
-    res.setHeader('Access-Control-Allow-Origin', origin); 
+module.exports = (req, res) => {
+  const allowedOrigins = ['https://jtsgrit0.github.io', 'http://localhost:3000', 'https://barzidorock.vercel.app'];
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
   }
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
   
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
   if (req.method === 'OPTIONS') {
-    if (allowedOrigins.includes(origin)) { 
-      res.setHeader('Access-Control-Allow-Origin', origin); 
-    }
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     return res.status(200).end();
   }
 
