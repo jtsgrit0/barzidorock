5acdaea53c8b2640e47afe14b3cbde91488c9bfc
Taewoo Kim <taewoo.kim@git-flow.com>
2026-07-04 08:58:59 +0900

    Fix splash screen icon path for deployment

--- client/src/components/SplashScreen.js
+++ client/src/components/SplashScreen.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import '../SplashScreen.css';
-import appIcon from './app_icon.png'; // app_icon.png를 직접 import
 
 const SplashScreen = ({ loading }) => {
   return (
     <div className={`splash-screen ${!loading ? 'hide' : ''}`}>
-      <img src={appIcon} alt="Loading..." className="splash-icon" />
+      <img src={`${process.env.PUBLIC_URL}/app_icon.png`} alt="Loading..." className="splash-icon" />
     </div>
   );
 };
