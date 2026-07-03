dcb30bf903294c3f23efd5693b31d21c0399e9a0
Taewoo Kim <taewoo.kim@git-flow.com>
2026-07-03 21:29:14 +0900

    Fix ESLint error - remove unused faCog import

--- client/src/components/TabBar.js
+++ client/src/components/TabBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faTicket, faCalendarAlt, faCog, faCrosshairs, faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faTicket, faCalendarAlt, faCrosshairs, faHeart } from '@fortawesome/free-solid-svg-icons';
 import { useTranslation } from 'react-i18next';
 import './TabBar.css';
 
