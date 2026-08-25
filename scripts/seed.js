61a3a5a52927bf2d956db6dc907bb0ae325784ef
Taewoo Kim <taewoo.kim@git-flow.com>
2026-08-25 16:59:54 +0900

    신촌 딥퍼플 관련 장소 추가

--- scripts/seed.js
+++ scripts/seed.js
@@ -83,6 +83,7 @@ async function seedInitialData() {
     allVenues = allVenues.concat(await fetchPlacesData("이태원 락바", "itaewon", "rock_bar"));
     allVenues = allVenues.concat(await fetchPlacesData("신촌 라이브 공연장", "sinchon", "live_venue"));
     allVenues = allVenues.concat(await fetchPlacesData("신촌 락바", "sinchon", "rock_bar"));
+    allVenues = allVenues.concat(await fetchPlacesData("신촌 딥퍼플", "sinchon", "live_venue"));
 
     const outputPath = path.join(__dirname, '../../client/public', 'venues.json');
     const outputDir = path.dirname(outputPath);
